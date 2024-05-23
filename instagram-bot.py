d011299f10701c191b115d45b8af0508d575eda2
ballursajeed <ballursajeed@gmail.com>
2024-05-23 14:23:04 +0530

    post a image on instagram in python

--- instagram-bot.py
+++ instagram-bot.py
@@ -2,16 +2,22 @@
 #username = "_nerd_n_gga"
 #password = "welcome7867"
 
-from selenium import webdriver
+'''from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import By '''
 import time
 
-def post_on_instagram(message, image_path):
-    username, password = "_nerd_n_gga", "welcome7867"
+import json
+import requests
+#Welcome7867    fbpageID=294285857108535  ig_user_id = 17841458305592495
+
+#accesss_token:EAAGkWZBMh3GUBO4dq6VZADIZApnedTxuiCA9NO9J4aHGsIEcrVmdyxxTJCP2cmp9lxUPTn7WqbikiT1E2YsPy1kTXt0wZAd3CcXklboeY6F0VnfB776E2AwX81NEeN4DNuZB3kTZBe3NcM1P0AK4zS2CETsexiJdajgzcmLZCvC3GDE3JmR0jU8ly8DSWkaYkFbFgD6lKpqNOrR1sOJZASLpm4pQOwZDZD
+#accessToken: EAAGkWZBMh3GUBO3hNbV3uaKZAubdJd8jQf5dgGsGcpZB7RTkByCmxqlLhXDXEZBwOjjgCN733PJvlqC1ZB3VwNiQV3WgtsVZBdwVrk9Qq6XwO1bIzQ5i70b2ZACY30IISjb3pPV9DiMt6DxVZBmCh6mhf8F42UrPdxYR63Camz3iKY0aHBb1HqsDZCZCAG9sxNzjTf
+'''def post_on_instagram(message, image_path):
+    username, password = "zipbag_786", "jarvisai"
 
     option = Options()
     option.add_argument("start-maximized")
@@ -50,10 +56,6 @@ def post_on_instagram(message, image_path):
         create_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@class='x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh x1n2onr6 x1plvlek xryxfnj x1c4vz4f x2lah0s xdt5ytf xqjyukv x1qjc9v5 x1oa3qoh x1nhvcw1']//svg[@aria-label='New post']")))
         create_button.click()
 
-
-
-
-
         # Wait for the file upload input to be present
         file_input = wait.until(EC.presence_of_element_located((By.XPATH, "//input[@type='file']")))
 
@@ -91,4 +93,37 @@ def post_on_instagram(message, image_path):
             driver.quit()
 
 # Example usage
-post_on_instagram("This is a test post!", "/path/to/your/image.jpg")
+post_on_instagram("This is a test post!", "/path/to/your/image.jpg")'''
+
+def post_image( imge_url):
+    access_token = "EAAGkWZBMh3GUBO3hNbV3uaKZAubdJd8jQf5dgGsGcpZB7RTkByCmxqlLhXDXEZBwOjjgCN733PJvlqC1ZB3VwNiQV3WgtsVZBdwVrk9Qq6XwO1bIzQ5i70b2ZACY30IISjb3pPV9DiMt6DxVZBmCh6mhf8F42UrPdxYR63Camz3iKY0aHBb1HqsDZCZCAG9sxNzjTf"
+    ig_user_id = "17841458305592495"
+    
+    post_url = 'https://graph.facebook.com/v20.0/{}/media'.format(ig_user_id)
+    payload = {
+        'image_url':imge_url,
+        'caption':'a test posting',
+        'access_token':access_token,
+    }
+    
+    r = requests.post(post_url,data = payload)
+    print(r.text)
+    print("Media uploaded successfully!")
+    
+    result = json.loads(r.text)
+    if 'id' in result:
+        creation_id = result['id']
+        second_url='https://graph.facebook.com/v20.0/{}/media_publish'.format(ig_user_id)
+        second_payload = {
+            'creation_id':creation_id,
+            'access_token':access_token,
+        }
+        r = requests.post(second_url,data=second_payload)
+        print(r.text)
+        print("image publish to intagram")
+    else:
+        print("image posting is not possible!")
+    
+imge_url = "https://pbs.twimg.com/media/GKF1pPiXcAE_KOc?format=jpg&name=small"   
+post_image(imge_url)
+time.sleep(20)
